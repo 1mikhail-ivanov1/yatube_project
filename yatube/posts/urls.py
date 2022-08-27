d91d55b5c477311628a472bd0b170a89911c164d
Михаил Иванов <misha_ivanov1999@mail.ru>
2022-08-27 15:01:33 +0300

    замена файла db.sqlite3

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
     # Главная страница
-    path('', views.index, name='main_page'),
-    path('group/<slug:any_slug>/', views.group_posts, name='group_list')
+    path('', views.index, name = 'index'),
+    path('group/<slug:slug>/', views.group_posts, name = 'group_list'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,27 @@
-from multiprocessing import context
+from turtle import title
 from django.shortcuts import render, get_object_or_404
 
-from yatube import posts
+
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by("-pub_date")[:10]
-    context = {'posts': posts,}
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Главная страница проекта yatube'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
-    context = {'group': group, 'posts': posts}
-    return render(request, 'posts/group.html', context)
+    title = 'Группы проекта yatube'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
